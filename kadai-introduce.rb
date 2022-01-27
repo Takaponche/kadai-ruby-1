d72ac2903438333ee2353260fc406de0efebf1de
taka.ponche <takaponche@gmail.com>
2022-01-27 07:32:45 +0000

    edit to 1 line

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -3,6 +3,4 @@ lastName = 'Amaya'
 age = 30
 
 #display name and age
-puts 'My name is ' + firstName + ' ' + lastName + '.'
-
-puts 'I am ' + age.to_s + ' years old.'
+puts 'My name is ' + firstName + ' ' + lastName + '. ' + ' I am ' + age.to_s + ' years old.'
